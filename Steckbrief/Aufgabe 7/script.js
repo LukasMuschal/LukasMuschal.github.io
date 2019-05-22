bbd8800838e702cd5b1057b0913e2a32e92c2906
LukasMuschal <muschal17@gmail.com>
2019-05-22 16:33:06 +0200

    c

--- Steckbrief/Aufgabe 7/script.js
+++ Steckbrief/Aufgabe 7/script.js
@@ -1,2 +1,10 @@
 console.log("Test1");
+window.onload = function name(params) {
+    console.log();
+    document.getElementById("Button").addEventListener("Drück mich", DoStuff);
+};
+function DoStuff() {
+    console.log();
+    document.getElementById("Button").innerHTML = "Aua, das hat weh getan!";
+}
 //# sourceMappingURL=script.js.map
