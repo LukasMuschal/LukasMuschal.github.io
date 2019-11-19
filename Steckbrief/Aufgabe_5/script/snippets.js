3d6a07794e8ee4b24c0e6bb9bddeb52dca1ef7ee
LukasMuschal <muschal17@gmail.com>
2019-11-19 12:27:00 +0100

    s

--- Steckbrief/Aufgabe_5/script/snippets.js
+++ Steckbrief/Aufgabe_5/script/snippets.js
@@ -1,40 +1,83 @@
-/**
- * Folgende Code-Fragmente untersützen Sie bei der Bearbeitung der Aufgabe
- *
- * Diese Fragmente werden wir in den kommenden Stunden noch vertiefen,
- * damit Sie aber etwas "greifbares" schaffen können, sollten Sie diese Fragmente jetzt schon einsetzen
-*/
-/**
- * A)
- * Den Inhalt eines DOM-Elements manipulieren / einen Wert in ein Selektiertes DOM-Element schreiben
- * Zwei Punkte müssten Sie anpassen:
- *   1. den Selektor (hier am Beispiel mit einem Klassenselektor)
- *   2. den Wert (hier eine Zeichenkette, kann aber natürlich auch eine Variante/mehrere Variablen sein)
- */
-document.querySelector('.exampleClassOfElement').innerHTML = "Dieser Text wurde zur Laufzeit in das Element geschrieben";
-/**
- * B)
- * Funktion
- * Eine Funktion ist ein Anweisungsblock, der bei Aufruf dieser Funktion abgearbeitet wird.
- * Bspw. wenn ein Button geklick wird, dann sollen die Anweisungen dieser Funktion aufgerufen werden.
- */
-function exampleFunctionName() {
-    // hier werden die Anweisungen, Zeile für Zeile, aufgeführt
-    // Beispiel (eine Variable wird deklariert. Der Inhalt der Variable wird als Prompt / Alert ausgegeben):
-    var test = 1;
-    alert(test);
-}
-/**
- * C)
- * Button klicken
- *
- * Das Event-Handling in TypeScript ist ein eigenes, großes Thema. Für den Anfang, damit Ihre kleine Anwendung
- * etwas an "Leben" gewinnt können Sie folgendermaßen eine Funktion / einen Anweisungsblock aufrufen:
- * In HTML ergänzen Sie an ein Element das Attribut "onclick". Als Wert wird der Funktionsname mit Klammer auf+zu
- * angegeben. Bspw. so:
- * <button onclick="exampleFunctionName()">Mein Button</button>
- *
- * Das "onclick" Attribut kann an sämtlichen HTML-Tags ergänzt werden, bspw.
- * <img src="meinBild.jpg" onclick="exampleFunctionName()">
- */
+var world = 32000;
+var word1 = "Die weltweiten Emissionsausstoße:";
+var empty = " ";
+var europa1 = "Europa";
+var europa2 = "Die Emission von Europa ist: ";
+var europa18 = 4209.3;
+var europa3 = "Relativ zur Gesamtemission der Welt verursacht Europa damit ";
+var europa4 = "Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um ";
+var europa08 = 4965.7;
+var europa5 = "2018 im Vergleich zu 2008 sind das ";
+console.log(word1);
+console.log(empty);
+console.log(europa1);
+console.log(europa2 + europa18 + " kg CO2 (Stand 2018)");
+console.log(europa3 + (europa18 / world * 100) + "%");
+console.log(europa4 + (100 * europa18 / europa08 - 100) + "% verändert");
+console.log(europa5 + (europa18 - europa08) + "kg CO2");
+var afrika1 = "Afrika";
+var afrika2 = "Die Emission von Afrika ist: ";
+var afrika18 = 1235.5;
+var afrika3 = "Relativ zur Gesamtemission der Welt verursacht Afrika damit ";
+var afrika4 = "Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um ";
+var afrika08 = 1028;
+var afrika5 = "2018 im Vergleich zu 2008 sind das ";
+console.log(empty);
+console.log(afrika1);
+console.log(afrika2 + afrika18 + " kg CO2 (Stand 2018)");
+console.log(afrika3 + (afrika18 / world * 100) + "%");
+console.log(afrika4 + (100 * afrika18 / afrika08 - 100) + "% verändert");
+console.log(afrika5 + (afrika18 - afrika08) + "kg CO2");
+var samerika1 = "Südamerika";
+var samerika2 = "Die Emission von Südamerika ist: ";
+var samerika18 = 1261.5;
+var samerika3 = "Relativ zur Gesamtemission der Welt verursacht Südamerika damit ";
+var samerika4 = "Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emission um ";
+var samerika08 = 1132.6;
+var samerika5 = "2018 im Vergleich zu 2008 sind das ";
+console.log(empty);
+console.log(samerika1);
+console.log(samerika2 + samerika18 + " kg CO2 (Stand 2018)");
+console.log(samerika3 + (samerika18 / world * 100) + "%");
+console.log(samerika4 + (100 * samerika18 / samerika08 - 100) + "% verändert");
+console.log(samerika5 + (samerika18 - samerika08) + "kg CO2");
+var namerika1 = "Nordamerika";
+var namerika2 = "Die Emission von Nordamerika ist: ";
+var namerika18 = 6035.6;
+var namerika3 = "Relativ zur Gesamtemission der Welt verursacht Nordamerika damit ";
+var namerika4 = "Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um ";
+var namerika08 = 6600.4;
+var namerika5 = "2018 im Vergleich zu 2008 sind das ";
+console.log(empty);
+console.log(namerika1);
+console.log(namerika2 + namerika18 + " kg CO2 (Stand 2018)");
+console.log(namerika3 + (namerika18 / world * 100) + "%");
+console.log(namerika4 + (100 * namerika18 / namerika08 - 100) + "% verändert");
+console.log(namerika5 + (namerika18 - namerika08) + "kg CO2");
+var asien1 = "Asien";
+var asien2 = "Die Emission von Asien ist: ";
+var asien18 = 16274.1;
+var asien3 = "Relativ zur Gesamtemission der Welt verursacht Asien damit ";
+var asien4 = "Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um ";
+var asien08 = 12954.7;
+var asien5 = "2018 im Vergleich zu 2008 sind das ";
+console.log(empty);
+console.log(asien1);
+console.log(asien2 + asien18 + " kg CO2 (Stand 2018)");
+console.log(asien3 + (asien18 / world * 100) + "%");
+console.log(asien4 + (100 * asien18 / asien08 - 100) + "% verändert");
+console.log(asien5 + (asien18 - asien08) + "kg CO2");
+var australien1 = "Australien";
+var australien2 = "Die Emission von Australien  ist: ";
+var australien18 = 2100.5;
+var australien3 = "Relativ zur Gesamtemission der Welt verursacht Australien damit ";
+var australien4 = "Für Australien hat sich 2018 im Vergleich zu 2008 die Emission um ";
+var australien08 = 1993;
+var australien5 = "2018 im Vergleich zu 2008 sind das ";
+console.log(empty);
+console.log(australien1);
+console.log(australien2 + australien18 + " kg CO2 (Stand 2018)");
+console.log(australien3 + (australien18 / world * 100) + "%");
+console.log(australien4 + (100 * australien18 / australien08 - 100) + "% verändert");
+console.log(australien5 + (australien18 - australien08) + "kg CO2");
 //# sourceMappingURL=snippets.js.map
