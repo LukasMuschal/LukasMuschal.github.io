5e635650989e8957ed084e8ab19351077d49a1f7
LukasMuschal <muschal17@gmail.com>
2019-12-03 10:55:13 +0100

    s

--- Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.js
+++ Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
     document.querySelector("#Pad1").addEventListener("click", playSample);
 });
 function playSample() {
-    var sound = new Audio("Sounds/kick.mp3");
+    var sound = new Audio("kick.mp3");
     sound.play();
 }
 //# sourceMappingURL=snippets_aufgabe_7.js.map
