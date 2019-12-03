339d91285de97f32ceaeab27fd2382e512773a77
LukasMuschal <muschal17@gmail.com>
2019-12-03 11:12:26 +0100

    x

--- Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.js
+++ Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.js
@@ -1,15 +1,8 @@
 window.addEventListener("load", function () {
-    document.querySelector("#Pad1").addEventListener("click", playSample);
+    document.querySelector("#Kick").addEventListener("click", playSample);
 });
 function playSample() {
-    var sound = new Audio("kick.mp3");
+    var sound = new Audio("Sounds/kick.mp3");
     sound.play();
 }
-window.addEventListener("load", function () {
-    document.querySelector("#Pad1").addEventListener("mousedown", function () { playSample("kick.mp3"); });
-    function playSample(mp3assets) {
-        var sound = new Audio(mp3assets);
-        sound.play();
-    }
-});
 //# sourceMappingURL=snippets_aufgabe_7.js.map
--- Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.ts
+++ Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.ts
@@ -1,18 +1,9 @@
 window.addEventListener("load", function(){
 
-    document.querySelector("#Pad1").addEventListener("click", playSample)
+    document.querySelector("#Kick").addEventListener("click", playSample)
 })
 
 function playSample() {
-    var sound:HTMLAudioElement = new Audio("kick.mp3");
-    sound.play();
-}
-
-window.addEventListener("load", function () {
-    document.querySelector("#Pad1").addEventListener("mousedown", function () { playSample("kick.mp3") });
-    
-
-function playSample(mp3assets: string) {
-    var sound: HTMLAudioElement = new Audio(mp3assets);
+    var sound:HTMLAudioElement = new Audio("Sounds/kick.mp3");
     sound.play();
 }
