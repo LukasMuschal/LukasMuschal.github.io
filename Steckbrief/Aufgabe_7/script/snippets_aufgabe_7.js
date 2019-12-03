919b305ec5c42d48652fe56e70933f690466bc41
LukasMuschal <muschal17@gmail.com>
2019-12-03 11:15:50 +0100

    s

--- Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.js
+++ Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.js
@@ -1,8 +1,10 @@
 window.addEventListener("load", function () {
     document.querySelector("#Kick").addEventListener("click", playSample);
+    document.querySelector("#Snare").addEventListener("click", playSample);
 });
 function playSample() {
     var sound = new Audio("Sounds/kick.mp3");
+    var sound = new Audio("Sounds/snare.mp3");
     sound.play();
 }
 //# sourceMappingURL=snippets_aufgabe_7.js.map
--- Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.ts
+++ Steckbrief/Aufgabe_7/script/snippets_aufgabe_7.ts
@@ -1,9 +1,10 @@
 window.addEventListener("load", function(){
-
     document.querySelector("#Kick").addEventListener("click", playSample)
+    document.querySelector("#Snare").addEventListener("click", playSample)
 })
 
 function playSample() {
     var sound:HTMLAudioElement = new Audio("Sounds/kick.mp3");
+    var sound:HTMLAudioElement = new Audio("Sounds/snare.mp3"); 
     sound.play();
 }
