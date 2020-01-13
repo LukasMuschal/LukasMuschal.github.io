2655641483476a2ce2d496fb4081c5da5fb417d3
LukasMuschal <muschal17@gmail.com>
2020-01-13 14:56:21 +0100

    a

--- Steckbrief/Aufgabe_9/Aufgabe_9.js
+++ Steckbrief/Aufgabe_9/Aufgabe_9.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", function () {
     var allTasks = [];
-    document.querySelector("#addTask").addEventListener("click", function () {
-        if (document.getElementById("inputField").value != " ") {
+    document.querySelector("#Hinzufügen").addEventListener("click", function () {
+        if (document.getElementById("Eingabe").value != " ") {
             var newTask = new Object;
-            newTask.taskName = document.getElementById("inputField").value;
+            newTask.taskName = document.getElementById("Eingabe").value;
             allTasks.push(newTask);
             console.log(allTasks.length);
-            document.getElementById("inputField").value = " ";
+            document.getElementById("Eingabe").value = " ";
             createElementsFromArray();
         }
     }, false);
@@ -16,7 +16,7 @@ window.addEventListener("load", function () {
     function createElementsFromArray() {
         document.getElementById("myTasks").innerHTML = " ";
         var div;
-        var _loop_1 = function (i) {
+        for (let i = 0; i < allTasks.length; i++) {
             console.log(allTasks[i]);
             div = document.createElement("div");
             div.setAttribute("class", "newDiv");
@@ -25,12 +25,9 @@ window.addEventListener("load", function () {
             document.getElementById(i.toString()).addEventListener("click", function () {
                 deleteObjFromArray(i);
             }, false);
-        };
-        for (var i = 0; i < allTasks.length; i++) {
-            _loop_1(i);
         }
-        var allCircles = document.getElementsByClassName("circle");
-        for (var y = 0; y < allCircles.length; y++) {
+        let allCircles = document.getElementsByClassName("circle");
+        for (let y = 0; y < allCircles.length; y++) {
             allCircles[y].addEventListener("click", function () {
                 checkItem(this);
             }, false);
